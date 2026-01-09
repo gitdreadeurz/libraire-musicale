26cf0548dae17144edbc17e4081e02d41ea71755
Yann <yannmonory@gmail.com>
2026-01-09 11:44:51 +0100

    correction search

--- scripts/search.sh
+++ scripts/search.sh
@@ -1,6 +1,6 @@
-echo "Indiquez une partie des paroles à rechercher"
-read lyrics
-echo "Voici le résultat suivant le chemin genre/artiste/chanson"
- grep -Ril --include="*.txt" "$lyrics"
+echo "Indiquez une partie des paroles à rechercher :"
+read -r lyrics
+cd ..
+echo "Voici le résultat suivant le chemin genre/artiste/chanson :"
 
-
+grep -Ril --include="*.txt" -i "$lyrics" 
