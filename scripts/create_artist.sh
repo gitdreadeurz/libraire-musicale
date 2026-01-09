cbe202c6f38e7beb1289713feb91c928ccabcf64
Yann <yannmonory@gmail.com>
2026-01-09 11:58:45 +0100

    create artist incassable

--- scripts/create_artist.sh
+++ scripts/create_artist.sh
@@ -1,6 +1,10 @@
-echo "Quel genre de musique?"
-read GENRE    
-echo "Nom de l'artiste?"
+echo "Quel genre de musique ?"
+read GENRE
+GENRE=$(echo "$GENRE" | tr '[:upper:]' '[:lower:]')
+
+echo "Nom de l'artiste ?"
 read artist_name
+artist_name=$(echo "$artist_name" | tr '[:upper:]' '[:lower:]')
+
 cd ..
-mkdir -p $GENRE/"$artist_name"
+mkdir -p "$GENRE/$artist_name"
